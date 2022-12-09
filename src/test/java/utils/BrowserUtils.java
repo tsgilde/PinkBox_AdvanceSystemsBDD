83b1143e27ddd9ef2216e71d233fc16b84f071b7
Cristinalc01 <mariaclozadac@gmail.com>
2022-12-08 21:12:42 -0300

    us-06

--- src/test/java/utils/BrowserUtils.java
+++ src/test/java/utils/BrowserUtils.java
@@ -50,34 +50,18 @@ public class BrowserUtils {
         }
     }
 
-    private static void initializeDriver(String browser) {
-        if (ConfigReader.readProperty("runInSaucelabs").equalsIgnoreCase("true")) {
-            String sauceUsername = "oauth-gilde.taita-2ed06";
-            String sauceKey = "1120ef61-c91d-4432-8828-bd98e7d09984";
-            String sauceURL = "https://" + sauceUsername + ":" + sauceKey + "@ondemand.us-west-1.saucelabs.com:443/wd/hub";
-
-            try {
-                DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-                capabilities.setCapability("version", "107");
-                capabilities.setCapability("platform", "Windows 11");
-                driver = new RemoteWebDriver(new URL(sauceURL), capabilities);
-            } catch (MalformedURLException e) {
-                throw new RuntimeException(e);
-            }
-
-        } else {
-            switch (browser) {
-                case "chrome":
-                    WebDriverManager.chromedriver().setup();
-                    driver = new ChromeDriver();
-                    break;
-                case "firefox":
-                    WebDriverManager.firefoxdriver().setup();
-                    driver = new FirefoxDriver();
-                    break;
-                default:
-                    System.out.println("Invalid browser name");
-            }
+    private static void initializeDriver(String browser){
+        switch (browser){
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+            default:
+                System.out.println("Invalid browser name");
         }
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
---
+++ src/test/resources/features/titlehomepage.feature
@@ -0,0 +1,10 @@
+Feature: title Home page test
+
+
+  @ASP-6
+  Scenario: Test header of the home page
+    Given I open url of homepage
+    Then Verify header text is "Advance Systems - Home"
+
+
+
