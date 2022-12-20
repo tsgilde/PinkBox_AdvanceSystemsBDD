06b7d197c3ac289659f9dc4ce15c768d9447e9b4
Claudia Lozada <claudia.lozada.c@gmail.com>
2022-12-20 12:56:56 -0500

    @ASP-34, @ASP-35

---
+++ src/test/java/pages/ServicesPage.java
@@ -0,0 +1,14 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utils.BrowserUtils;
+
+public class ServicesPage {
+    public ServicesPage() {
+        PageFactory.initElements(BrowserUtils.getDriver(), this);
+    }
+    @FindBy(xpath = "//h1")
+    public WebElement title;
+}
